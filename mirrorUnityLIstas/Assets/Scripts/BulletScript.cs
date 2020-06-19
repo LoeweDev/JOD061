23805424423862a94f8bcdcd5dc05fcf5f670c0c
Fenzl <leongamedev@outlook.com>
2020-06-19 20:22:27 -0300

    Commit Ex 4
    Lista 4

---
+++ mirrorUnityLIstas/Assets/Scripts/BulletScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using Mirror;
+
+public class BulletScript : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject,1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0f,0f,Time.deltaTime * 25f);
+    }
+
+    [ServerCallback]
+    private void OnTriggerEnter(Collider other)
+    {
+        NetworkServer.Destroy(gameObject);
+        NetworkServer.Destroy(other.gameObject);
+    }
+}
---
+++ mirrorUnityLIstas/Assets/Scripts/Movement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Movement : NetworkBehaviour
+{
+    public float speed,rotSpeed;
+    float moveForward,moveSides;
+    public Transform arma;
+    private Rigidbody body;
+    public GameObject projectile;
+    private Material materialRef;
+    
+    // Start is called before the first frame update   
+    void Start()
+    {
+        arma = GetComponentInChildren<Transform>();
+        body = GetComponent<Rigidbody>();
+        materialRef = GetComponent<Material>();
+        materialRef.color = new Color (Random.Range(0f,1f), Random.Range(0f,1f),Random.Range(0f,1f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveForward,moveSides;
+        moveForward = Input.GetAxis("Vertical")*Time.deltaTime;
+        moveSides = Input.GetAxis("Horizontal")*Time.deltaTime;
+        
+        if(Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            CmdAtirar();
+        }
+
+        if (isLocalPlayer)
+        {
+            body.velocity = new Vector3 (0f,0f,moveForward)*speed;
+            transform.Rotate(0f,moveSides*rotSpeed,0f);
+        }
+        if (!isLocalPlayer)
+        return;
+    }
+
+    void CmdAtirar()
+    {
+        GameObject projetil = Instantiate(projectile, arma.position, transform.rotation);
+        NetworkServer.Spawn(projetil);
+    }
+}
