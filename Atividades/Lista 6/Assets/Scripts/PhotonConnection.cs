0098e428d63a0bbe40a9100b1d122b66e9ec386c
Fenzl <leongamedev@outlook.com>
2020-06-20 23:33:57 -0300

    Commit GERAL
    Commit das 3 listas

---
+++ Atividades/Lista 6/Assets/Scripts/PhotonConnection.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PhotonConnection : MonoBehaviourPunCallBacks
+{
+    public InputField inputField;
+    public String gameVersion,roomName;
+    public Button button;
+    private byte maxPlayers;
+
+    void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    void Start()
+    {
+        gameVersion = "0.0.0.1";
+        roomName = "First Game Room";
+        maxPlayers = 10;
+    }
+
+    public void Connect()
+    {   
+        if (!PhotonNetwork.IsConnected)
+        {
+            input.interactable = false;
+            button.interactable = false;
+            PhotonNetwork.NickName = input.text;
+            PhotonNetwork.GameVersion = this.gameVersion;
+            PhotonNetwork.ConnectUsingSettings();
+            return;
+        }
+        if (PhotonNetwork.CountOfRooms == 0)
+        {
+            button.interactable = false;
+            RoomOptions options = new RoomOptions();
+            options.MaxPlayers = this.maxPlayers;
+            PhotonNetwork.JoinOrCreateRoom(this.roomName, options, TypedLobby.Default);
+            return;
+        }
+
+        PhotonNetwork.JoinRoom(this.roomName);
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        button.interactable = true;
+        button.GetComponentInChildren<Text>().text = "Jogar";
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("GameScene");
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Atividades/PUN-Lista 5/Assets/Scripts/PhotonConnectScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PhotonConnectScript : MonoBehaviourPunCallbacks
+{
+
+    string gameVersion = "0.0.1";
+    string roomName = "JOD061";
+    string nickName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nickName = "Player";
+        Debug.Log ("Conectando no servidor...",this);
+        PhotonNetwork.GameVersion = gameVersion;
+        PhotonNetwork.NickName = nickName + Random.Range(0, 9999);
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster() 
+    {
+        RoomOptions options = new RoomOptions();
+
+        Debug.Log("Conectado!", this);
+        if (PhotonNetwork.CountOfRooms == 0) 
+        {
+            options.MaxPlayers = 4;
+            PhotonNetwork.JoinOrCreateRoom(roomName, options, TypedLobby.Default);
+        } else {
+            PhotonNetwork.JoinRoom(roomName);
+        }
+    }
+
+    public override void OnCreatedRoom() 
+    {
+        Debug.Log("Criada a sala " + roomName);
+    }
+
+    void OnPlayerEnteredRoom()
+    {
+         Debug.Log("Jogador " + PhotonNetwork.LocalPlayer.NickName + " entrou na sala " + roomName + "(" + PhotonNetwork.CurrentRoom.PlayerCount + ")");
+    }
+
+    void OnPlayerLeftRoom()
+    {
+         Debug.Log("Jogador " + PhotonNetwork.LocalPlayer.NickName + " saiu da sala " + roomName + "(" + PhotonNetwork.CurrentRoom.PlayerCount + ")");
+    }
+
+    public override void OnDisconnected(Photon.Realtime.DisconnectCause cause) 
+    {
+        Debug.Log("Desconectado!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            
+    }
+}
